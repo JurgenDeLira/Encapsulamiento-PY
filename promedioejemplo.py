299009b51235091bcdda8af3adb796a5ab978323
Jürgen De Lira <106788934+JurgenDeLira@users.noreply.github.com>
2024-07-22 10:51:59 -0700

    Add files via upload

---
+++ promedioejemplo.py
@@ -0,0 +1,11 @@
+def Promedio(listaNumeros):
+  total=0.0
+  elementos=0
+  for numero in listaNumeros:
+    total+=numero
+    elementos+=1
+  promedio=total/elementos
+  return promedio
+
+def Promedio(listaNumeros):
+  total = float(0.0)
